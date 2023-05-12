80da5395a3f6e712fdd5a5726459e6f186996875
PhongNarin <phong.tran@southasiaservices.com>
2023-05-12 09:57:37 +0700

    feat(user): update user

--- src/modules/posts/posts.repository.ts
+++ src/modules/posts/posts.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { FilterQuery, Model, PopulateOptions, Types } from 'mongoose';
 
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,6 +8,7 @@ import {
   Param,
   Post,
   Put,
+  Query,
   Req,
   UploadedFile,
   UseGuards,
@@ -19,7 +20,7 @@ import { DecodedIdToken } from 'firebase-admin/auth';
 import { Types } from 'mongoose';
 
 import { configuration } from 'src/config';
-import { UpdateUserDto, UserDto } from 'src/dto';
+import { PaginateDto, UpdateUserDto, UserDto } from 'src/dto';
 import { FileSizeValidationPipe, ParseMongooseObjectID } from 'src/pipe';
 import { TRequestWithToken } from 'src/types';
 import { storageUploadHandle } from 'src/utils';
@@ -64,18 +65,33 @@ export class UsersController {
 
   @UseGuards(FirebaseAuthGuard)
   @Get('profile/list-post')
-  async getListPost(@Req() { user: { uid } }: { user: DecodedIdToken }) {
+  async getListPost(
+    @Req() { user: { uid } }: { user: DecodedIdToken },
+    @Query() { page, limit }: PaginateDto,
+  ) {
     const postsOfUser = await this.usersService.getListPostOfUser(uid);
 
-    return postsOfUser.sort(
-      (postbefore: any, postafter: any) =>
-        postafter.createdAt - postbefore.createdAt,
-    );
+    const totalPost = postsOfUser.length;
+    const totalPage =
+      Math.ceil(totalPost / limit) > 1 ? Math.ceil(totalPost / limit) : 1;
+
+    const startIndex = (page - 1) * limit;
+
+    return {
+      posts: postsOfUser.slice(startIndex, startIndex + limit),
+      totalPost,
+      totalPage,
+      page,
+      limit,
+    };
   }
 
   @UseGuards(FirebaseAuthGuard)
   @Get('profile/list-post-receive')
-  async getListPostReceive(@Req() { user: { uid } }: TRequestWithToken) {
+  async getListPostReceive(
+    @Req() { user: { uid } }: TRequestWithToken,
+    @Query() { page, limit }: PaginateDto,
+  ) {
     const user = await this.usersService.getUserByUid(uid);
 
     const posts = await this.postsService.getAllPost();
@@ -86,12 +102,27 @@ export class UsersController {
         post.status === EPostStatus.IS_RECEIVED,
     );
 
-    return postUserHaveRequest;
+    const totalPost = postUserHaveRequest.length;
+    const totalPage =
+      Math.ceil(totalPost / limit) > 1 ? Math.ceil(totalPost / limit) : 1;
+
+    const startIndex = (page - 1) * limit;
+
+    return {
+      posts: postUserHaveRequest.slice(startIndex, startIndex + limit),
+      totalPost,
+      totalPage,
+      page,
+      limit,
+    };
   }
 
   @UseGuards(FirebaseAuthGuard)
   @Get('profile/list-post-request')
-  async getListPostRequest(@Req() { user: { uid } }: TRequestWithToken) {
+  async getListPostRequest(
+    @Req() { user: { uid } }: TRequestWithToken,
+    @Query() { page, limit }: PaginateDto,
+  ) {
     const user = await this.usersService.getUserByUid(uid);
 
     const posts = await this.postsService.getAllPost();
@@ -102,21 +133,35 @@ export class UsersController {
       (req) => req.userId.toString() === user._id.toString(),
     );
 
-    const postHaveRequest = posts.map((post) => {
-      let isReturn = false;
-      let myRequest;
-      for (const request of requestOfUser) {
-        myRequest = post.listRequest.find(
-          (req) => req._id.toString() === request._id.toString(),
-        );
+    const postHaveRequest = posts
+      .map((post) => {
+        let isReturn = false;
+        let myRequest;
+        for (const request of requestOfUser) {
+          myRequest = post.listRequest.find(
+            (req) => req._id.toString() === request._id.toString(),
+          );
 
-        if (myRequest) isReturn = true;
-      }
+          if (myRequest) isReturn = true;
+        }
 
-      if (isReturn) return { post, myRequest };
-    });
+        if (isReturn) return { post, myRequest };
+      })
+      .filter(Boolean);
 
-    return postHaveRequest.filter(Boolean);
+    const totalPost = postHaveRequest.length;
+    const totalPage =
+      Math.ceil(totalPost / limit) > 1 ? Math.ceil(totalPost / limit) : 1;
+
+    const startIndex = (page - 1) * limit;
+
+    return {
+      posts: postHaveRequest.slice(startIndex, startIndex + limit),
+      totalPost,
+      totalPage,
+      page,
+      limit,
+    };
   }
 
   @UseGuards(FirebaseAuthGuard)
