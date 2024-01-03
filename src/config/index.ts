0423097337694a479844ea0dc8ff54160a3978fd
PhongNarin <phong.tran@southasiaservices.com>
2024-01-03 08:09:32 +0700

    feat(firebase): update firebase

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,18 +9,8 @@ export type TConfiguration = {
   port: number;
   baseUrl: string;
   mongoURI: string;
-  firebaseDatabaseUrl: string;
-  // firebase admin cert
-  firebaseType: string;
-  firebaseProjectId: string;
-  firebasePrivateKeyId: string;
-  firebasePrivateKey: string;
-  firebaseClientEmail: string;
-  firebaseClientId: string;
-  firebaseAuthUri: string;
-  firebaseTokenUri: string;
-  firebaseAuthProvider: string;
-  firebaseClientX509CertUrl: string;
+  firebaseCert: string
+  firebaseDatabaseURL: string
 };
 
 export type TConfigService = ConfigService<TConfiguration>;
@@ -29,15 +19,6 @@ export const configuration = (): TConfiguration => ({
   port: +process.env.PORT,
   baseUrl: process.env.BASE_URL,
   mongoURI: process.env.MONGO_URI,
-  firebaseDatabaseUrl: process.env.FIREBASE_DATABASE_URL,
-  firebaseType: process.env.FIREBASE_TYPE,
-  firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-  firebasePrivateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-  firebasePrivateKey: process.env.FIREBASE_PRIVATE_KEY,
-  firebaseClientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  firebaseClientId: process.env.FIREBASE_CLIENT_ID,
-  firebaseAuthUri: process.env.FIREBASE_AUTH_URI,
-  firebaseTokenUri: process.env.FIREBASE_TOKEN_URI,
-  firebaseAuthProvider: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  firebaseClientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  firebaseCert: process.env.FIREBASE_CERT,
+  firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
 });
--- src/modules/firebase/firebase.service.ts
+++ src/modules/firebase/firebase.service.ts
@@ -1,69 +1,29 @@
 import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import {join} from 'path'
 
 import firebaseAdmin from 'firebase-admin';
 import { DecodedIdToken, UserRecord } from 'firebase-admin/lib/auth';
 
-import { TConfigService, configuration } from 'src/config';
-import { EUserRoles } from 'src/enums';
+import { TConfigService } from 'src/config';
 
 import { AuthErrorConstants } from '../auth';
 
 @Injectable()
 export class FirebaseService {
   private readonly admin: firebaseAdmin.app.App;
-  private database: firebaseAdmin.firestore.Firestore;
-
   constructor(
     @Inject(ConfigService)
     private configService: TConfigService,
   ) {
-    const serviceAccount = this.getFirebaseJson();
-
     this.admin = firebaseAdmin.initializeApp({
-      credential: firebaseAdmin.credential.cert(
-        serviceAccount as firebaseAdmin.ServiceAccount,
-      ),
-      databaseURL: this.configService.get('firebaseDatabaseUrl'),
+      credential: firebaseAdmin.credential.cert(join(__dirname, '..', '..', '..', this.configService.get('firebaseCert'))),
+      databaseURL: this.configService.get('firebaseDatabaseURL'),
     });
 
-    this.database = this.admin.firestore();
+    this.admin.firestore();
   }
 
-  private getFirebaseJson() {
-    const {
-      firebaseType,
-      firebasePrivateKey,
-      firebaseClientX509CertUrl,
-      firebaseClientEmail,
-      firebaseProjectId,
-      firebasePrivateKeyId,
-      firebaseClientId,
-      firebaseAuthUri,
-      firebaseTokenUri,
-      firebaseAuthProvider,
-    } = configuration();
-
-    return {
-      type: firebaseType,
-      project_id: firebaseProjectId,
-      private_key_id: firebasePrivateKeyId,
-      private_key: firebasePrivateKey,
-      client_email: firebaseClientEmail,
-      client_id: firebaseClientId,
-      auth_uri: firebaseAuthUri,
-      token_uri: firebaseTokenUri,
-      auth_provider_x509_cert_url: firebaseAuthProvider,
-      client_x509_cert_url: firebaseClientX509CertUrl,
-    };
-  }
-
-  private async updateCustomClaim(
-    uid: string,
-    customClaim: { customRole: EUserRoles },
-  ): Promise<void> {
-    await this.admin.auth().setCustomUserClaims(uid, customClaim);
-  }
 
   public async verifyToken(token: string): Promise<DecodedIdToken> {
     try {
